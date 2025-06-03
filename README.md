58f890601a2a5a5be68c13074356d75ff4af2035
Lucas de Faria Polaquini <cmte.lucas@hotmail.com>
2025-06-03 16:03:37 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Como configurar e executar o projeto
+
+Clone este repositório. Depois disso, faça o seguinte:
+
+Verifique se a API está ativa e rodando: http://localhost:8080/current-time
+
+Métricas da API: http://localhost:8080/metrics
+
+Verifique se o target está ativo no Prometheus: http://localhost:9090/targets  
+
+Acesse o Grafana e adicione uma nova fonte de dados para o Prometheus:  
+url: https://prometheus:9090
+
+Adicione dashboards: https://grafana.com/grafana/dashboards/10427-prometheus-net/
---
+++ my-api/Program.cs
@@ -0,0 +1,39 @@
+using Prometheus;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.UseHttpClientMetrics(); 
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseMetricServer();
+app.UseHttpMetrics();
+
+app.MapGet("/current-time", () =>
+{
+    var utcNow = DateTime.UtcNow;
+    var localNow = DateTime.Now;
+    string greeting = utcNow.Hour switch
+    {
+        >= 5 and < 12 => "Bom dia!",
+        >= 12 and < 18 => "Boa tarde!",
+        _ => "Boa noite!"
+    };
+
+    return Results.Ok(new
+    {
+        greeting,
+        utcNow = utcNow.ToString("yyyy-MM-dd HH:mm:ss 'UTC'"),
+        localNow = localNow.ToString("yyyy-MM-dd HH:mm:ss"),
+        timezone = TimeZoneInfo.Local.DisplayName
+    });
+});
+
+app.Run();
